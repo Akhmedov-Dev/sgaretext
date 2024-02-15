fe1124a02254169effc8809549786751daeddd83
AkhmedovDev <152636243+Akhmedov-Dev@users.noreply.github.com>
2024-02-15 19:54:05 +0500

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,95 @@
+import { signOut } from "firebase/auth"
+import { auth } from "../firebase"
+import { useNavigate } from "react-router-dom"
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { IoSend } from "react-icons/io5";
+import { addDoc, collection, onSnapshot } from 'firebase/firestore';
+import { useEffect, useState } from 'react'
+import { db } from '../firebase';
+
+export const Home = () => {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [newDate, setNewDate] = useState(new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds());
+  
+  const dbValue = collection(db, 'Users');
+
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    await addDoc(dbValue, { name: name, surname: surname, time: newDate });
+    console.log(name, surname, time);
+  }
+  const navigate = useNavigate();
+  const { dispatch } = useContext(AuthContext)
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch({ type: "LOGOUT", value: null })
+        navigate('/signin')
+      }).catch((error) => {
+        console.log(error);
+      })
+  }
+  useEffect(() => {
+    onSnapshot(
+      dbValue,
+      (snapshot) => {
+        let userlist = [];
+        snapshot.docs.forEach((doc) => {
+          userlist.push({ id: doc.id, ...doc.data() })
+        })
+        setData(userlist)
+      }
+    ), (error) => {
+      console.log(error);
+    }
+
+    console.log(data);
+  }, []);
+
+
+
+  return (
+    <>
+      <div>
+        <div className="div-h1">
+          <h1>Send your text</h1>
+        </div>
+        <div className='outpad'>
+          <div className="container">
+            {data.map((user) => {
+              return (
+                <div className="users flex flex-wrap" key={user.id}>
+                  <div className="user p-4">
+                    <div className="border border-gray-700 border-opacity-75 p-6 rounded-lg">
+                      <h2> {user.name}  : <span>{user.surname}</span></h2>
+                      <h2>{user.time  }</h2>
+                      <button onClick={() => {
+                        navigator.clipboard.writeText(user.surname)
+                      }}>Copy</button>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        <div className='send-div'>
+          <input className='inp-name' type="text" onChange={(event) => setName(event.target.value)} placeholder='Write your name' />
+
+          <input className='inp-text' type="text" onChange={(event) => setSurname(event.target.value)} placeholder='Write your text' />
+
+          <button className='button-send' onClick={handleCreate}><IoSend size="30" /></button>
+          <button className="signout-btn" onClick={handleSignOut}>Chiqish</button>
+        </div>
+      </div>
+    </>
+
+
+
+  )
+}
+
+
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfpg6cR4le6Y0xDykImldDb5IGbYbVABM",
+  authDomain: "homework-6a4c7.firebaseapp.com",
+  projectId: "homework-6a4c7",
+  storageBucket: "homework-6a4c7.appspot.com",
+  messagingSenderId: "945788651445",
+  appId: "1:945788651445:web:61fdc9afb582ee7aebbbfc",
+  measurementId: "G-M9K13GXKYG"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
